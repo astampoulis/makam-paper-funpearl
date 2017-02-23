11202eadebfa02ebc482f8efd04fe89900546db5
Antonis Stampoulis <antonis.stampoulis@originate.com>
2017-02-23 18:39:43 -0500

    switch to auto-generation from Markdown files

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 PDFLATEX=pdflatex -output-directory=tmp/
 BIBTEX=bibtex
 
-
 all:
 	@ mkdir -p tmp/
+	@ ./literate_makam_to_latex.sh impl/examples/paper tmp
 	@ $(PDFLATEX) main.tex
 	@ $(BIBTEX) tmp/main.aux
 	@ $(PDFLATEX) main.tex
@@ -16,3 +16,9 @@ clean:
 
 tmpclean:
 	@ rm -rf tmp/
+
+watch:
+	@ shared/watch-pdflatex -c main.tex
+	@ shared/watch-pdflatex main.tex
+
+.PHONY: all clean tmpclean watch
---
+++ literate_makam_to_latex.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+function usage() {
+  echo "Usage: $0 <input-dir> <output-dir>"
+  exit 1
+}
+
+if [[ "$#" -lt 2 ]]; then
+  usage
+fi
+
+INPUT_DIR=$1
+OUTPUT_DIR=$2
+
+for INPUT_FILE in $INPUT_DIR/*.md; do
+  OUTPUT_FILE="$OUTPUT_DIR/$(basename $INPUT_FILE .md).tex"
+  echo "Generating $OUTPUT_FILE from $INPUT_FILE"
+  pandoc -S $INPUT_FILE -o $OUTPUT_FILE
+  sed -i \
+      -e 's/λ/\\ensuremath{\\lambda}/g' \
+      -e 's/ω/\\ensuremath{\\omega}/g' \
+      -e 's/-\\textgreater{}/\\ensuremath{\\to}/g' \
+      -e 's/->/\\ensuremath{\\to}/g' \
+      -e 's/=\\textgreater{}/\\ensuremath{\\Rightarrow}/g' \
+      -e 's/=>/\\ensuremath{\\Rightarrow}/g' \
+      $OUTPUT_FILE
+done
